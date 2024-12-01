6baa5e19398f320ad3ea96d0ce5fbaa343860032
ONUR ALP AKIN <onur.alp.akin@proton.me>
2024-12-01 20:38:41 +0300

    call start stop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from flask import Flask, jsonify, request
-import threading
+from flask import Flask, jsonify
 import bot  # Import the bot module
 
 app = Flask(__name__)
@@ -7,27 +6,15 @@ app = Flask(__name__)
 @app.route("/start_bot", methods=["POST"])
 def start_bot():
     """Start the trading bot."""
-    if bot.bot_running:
-        return jsonify({"message": "Bot is already running."}), 400
-
-    bot.bot_running = True
-    bot.bot_thread = threading.Thread(target=bot.trading_bot)
-    bot.bot_thread.start()
-
-    return jsonify({"message": "Bot started successfully."})
+    response, status = bot.start_bot()
+    return jsonify(response), status
 
 
 @app.route("/stop_bot", methods=["POST"])
 def stop_bot():
     """Stop the trading bot."""
-    if not bot.bot_running:
-        return jsonify({"message": "Bot is not running."}), 400
-
-    bot.bot_running = False
-    if bot.bot_thread:
-        bot.bot_thread.join()
-
-    return jsonify({"message": "Bot stopped successfully."})
+    response, status = bot.stop_bot()
+    return jsonify(response), status
 
 
 @app.route("/status", methods=["GET"])
